275e0aea6264e7d3c6686475c728d51743fb11b4
Peter <trompeten.peter@gmx.de>
2018-11-29 20:55:21 +0100

    Optimasiation

--- PLLEngine/src/com/PLLEngine/Scene/layerComponents/Background.java
+++ PLLEngine/src/com/PLLEngine/Scene/layerComponents/Background.java
@@ -1,6 +1,7 @@
 package com.PLLEngine.Scene.layerComponents;
 
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 import com.PLLEngine.Game.Game;
@@ -9,19 +10,21 @@ import com.PLLEngine.srcLoader.SrcLoader;
 public class Background extends LayerComponent {
 	private SrcLoader loader;
 	private String src;
+	private BufferedImage bImage;
 	public Background(String src) {
 		 loader = new SrcLoader();
 		 this.src = src;
-	}
-
-	@Override
-	public void draw(Graphics g) {
 		try {
-			g.drawImage(loader.Image(src), 0, 0, Game.gwindow.width,Game.gwindow.height, null);
+			bImage=  loader.Image(src);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	@Override
+	public void draw(Graphics g) {
+			g.drawImage(bImage, 0, 0, Game.gwindow.width,Game.gwindow.height, null);
 
 	}
 
