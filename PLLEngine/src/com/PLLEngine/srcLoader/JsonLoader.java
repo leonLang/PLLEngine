7b0b4f417cdbc0c3f33eba1f8e26940ab57d813d
Peter <trompeten.peter@gmx.de>
2018-11-30 17:27:05 +0100

    Json loader aktualisiert

--- PLLEngine/src/com/PLLEngine/srcLoader/JsonLoader.java
+++ PLLEngine/src/com/PLLEngine/srcLoader/JsonLoader.java
@@ -1,5 +1,20 @@
 package com.PLLEngine.srcLoader;
 
-public class JsonLoader {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
+import com.PLLEngine.Scene.Map.Map;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonLoader {
+	public Map loadMap() throws JsonParseException, JsonMappingException, IOException {
+		Map map;
+		byte[] jsonData = Files.readAllBytes(Paths.get(""));
+		ObjectMapper objectMapper = new ObjectMapper();
+		map = objectMapper.readValue(jsonData, Map.class);
+		return map;
+	}
 }
