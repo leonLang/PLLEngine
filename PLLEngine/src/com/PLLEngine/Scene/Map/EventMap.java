685d463276100568836f45defa82fc7ad84d84f8
peterlang <peterlang@peters-macbook-pro.home>
2019-03-05 21:11:58 +0100

    Event stuff

--- PLLEngine/src/com/PLLEngine/Scene/Map/EventMap.java
+++ PLLEngine/src/com/PLLEngine/Scene/Map/EventMap.java
@@ -2,12 +2,37 @@ package com.PLLEngine.Scene.Map;
 
 public class EventMap {
 	private int[][] eventCoordinates;
+	private int yArray;
 	public EventMap(int[][] cArray) {
 		this.eventCoordinates = cArray;
-	}
+		this.yArray = this.eventCoordinates.length;
+		}
 	
 	public int[][] getEventCoordinates() {
 		return this.eventCoordinates;
 	}
-	
+	public int getEventTrigger(int x,int y) {
+		for(int i = 0;i < yArray;i++) {
+			if(this.eventCoordinates[i][0]== x) {
+				if(this.eventCoordinates[i][1]== y) {
+					return i;
+				}
+			}
+		}
+	return -1;	
+	}
+	//same but with a range within the target
+	public int getEventTrigger(int x,int y,int r) {
+		for(int i = 0;i < yArray;i++) {
+			if(this.eventCoordinates[i][0] > x+r && this.eventCoordinates[i][0] < x-r) {
+				if(this.eventCoordinates[i][1] > y+r && this.eventCoordinates[i][1] < y-r) {
+					return i;
+				}
+			}
+		}
+	return -1;	
+	}
+	public void triggerEvent(int i) {
+		System.err.println("no event found on position " + i);
+	}
 }
