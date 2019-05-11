d96334f25661655702ee6005c9aacf62ef69eee5
Leon <Leon@DESKTOP-SIC4F8N.fritz.box>
2019-05-11 15:24:21 +0200

    Lebensklasse verändert

--- PLLEngine/src/com/PLLEngine/Scene/layerComponents/entity/enemy/Health.java
+++ PLLEngine/src/com/PLLEngine/Scene/layerComponents/entity/enemy/Health.java
@@ -1,9 +1,33 @@
 package com.PLLEngine.Scene.layerComponents.entity.enemy;
 
 public class Health {
-	private int health;
+	private int lives;
+	private boolean deathStat;
 
-private void death() {
-	
-}
+	public Health(int lives) {
+		this.lives = lives;
+	}
+
+	private void death() {
+		if (lives >= 0) {
+			deathStat = true;
+		}
+	}
+
+	public void removeOneHeart() {
+		lives--;
+	}
+
+	public void instantKill() {
+		lives = 0;
+	}
+
+	public boolean isDeath() {
+		death();
+		return deathStat;
+	}
+
+	public int getLives() {
+		return lives;
+	}
 }
