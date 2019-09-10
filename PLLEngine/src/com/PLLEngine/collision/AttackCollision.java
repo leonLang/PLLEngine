eae232279b34ca69fcd9975ed1ff7025df29b326
Leon <Leon@DESKTOP-6QMOI8A.fritz.box>
2019-09-10 15:22:29 +0200

    Needed to commit again

--- PLLEngine/src/com/PLLEngine/collision/AttackCollision.java
+++ PLLEngine/src/com/PLLEngine/collision/AttackCollision.java
@@ -6,13 +6,20 @@ public class AttackCollision {
 	private int heightShot;
 	private int xShot;
 	private int yShot;
-	private int widthPlayer;
-	private int heightPlayer;
-	private int xPlayer;
-	private int yPlayer;
+	private int widthPlayer = 64;
+	private int heightPlayer = 64;
+	private int xPlayer = 560;
+	private int yPlayer = 362;
 
 	public AttackCollision(int xShot, int yShot, int widthShot, int heightShot) {
 		this.xShot = xShot;
 		this.yShot = yShot;
+		this.widthShot = widthShot;
+		this.heightShot = heightShot;
+	}
+
+	public boolean seeIfCollisionWithPlayer() {
+		Collision co = new Collision(xShot, yShot, widthShot, heightShot, xPlayer, yPlayer, widthPlayer, heightPlayer);
+		return co.Coll1();
 	}
 }
